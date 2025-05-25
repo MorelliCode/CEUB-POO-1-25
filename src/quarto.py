911ef914f31cdeb7683b8cefe010cd4249fe33a6
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-25 16:31:08 -0300

    Add skeleton for "Quarto" class

---
+++ src/quarto.py
@@ -0,0 +1,15 @@
+class Quarto:
+    def __init__(self, numero, tipo):
+        self.__numero = numero
+        self.__tipo = tipo
+        self.__disponivel = True
+
+    def reservar():
+        pass
+
+    def liberar():
+        pass
+
+    def esta_disponivel():
+        pass
+    
