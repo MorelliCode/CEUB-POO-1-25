2dc9e466311f16b6eb298efbcce6643ede22bb58
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-25 16:28:03 -0300

    Add skeleton of "Hotel" class

---
+++ src/hotel.py
@@ -0,0 +1,18 @@
+class Hotel:
+    def __init__(self):
+        self.__quartos = []
+        self.__hospedes = []
+        self.__reservas = []
+
+    def add_quarto(quarto):
+        pass
+
+    def remover_quarto(quarto):
+        pass
+
+    def registrar_hospede(hospede):
+        pass
+
+    def cancelar_reserva(reserva):
+        pass
+    
