a8e10b1bffd0dbde384e57f296d780f0610a11e2
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-25 17:48:11 -0300

    Add str method to Reserva class

--- src/reserva.py
+++ src/reserva.py
@@ -2,3 +2,6 @@ class Reserva:
     def __init__(self, hospede, quarto):
         self.__hospede = hospede
         self.__quarto = quarto
+
+    def __str__(self):
+        f"reserva quarto: {self.__quarto}, hóspede: {self.__hospede}"
