85241341adea068a14998dabc781e51a5677ddae
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-25 16:20:07 -0300

    Add skeleton for "funcionario" class

---
+++ src/funcionario.py
@@ -0,0 +1,17 @@
+from pessoa import Pessoa
+
+class Funcionario(Pessoa):
+    def __init__(self, id, nome, email):
+        super().__init__(id, nome, email)
+
+    def add_quarto(hotel, quarto):
+        pass
+
+    def remover_quarto(hotel, quarto):
+        pass
+
+    def registrar_hospede(hotel, hospede):
+        pass
+
+    def cancelar_reserva(hotel, reserva):
+        pass
