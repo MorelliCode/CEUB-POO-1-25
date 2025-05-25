c226168cd26d53ef6a070700556ec17f6e985adf
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-25 18:20:07 -0300

    Return main to skeleton

--- src/main.py
+++ src/main.py
@@ -4,11 +4,3 @@ from hospede import Hospede
 from hotel import Hotel
 from quarto import Quarto
 from reserva import Reserva
-
-list_people = []
-for person in range(3):
-    new_person = Pessoa(1, "new", "new@gmail.com")
-    list_people.append(new_person)
-
-for person in list_people:
-    print(person)
