c293867b4512938a2c69c90b3e1f020cf82a3b53
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-28 14:16:42 -0300

    Add string method

--- proj3/reserva.py
+++ proj3/reserva.py
@@ -2,3 +2,6 @@ class Reserva():
     def __init__(self, hospede:object, quarto:object):
         self.__hospede = hospede
         self.__quarto = quarto
+
+    def __str__(self):
+        return f"Reserva do {self.__quarto} por {self.__hospede}"
