81726cd9f057ac1b9d7167136e51ed07389dd502
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-28 09:15:43 -0300

    Update methods

--- proj3/hotel.py
+++ proj3/hotel.py
@@ -11,9 +11,11 @@ class Hotel():
 
     def add_reserva(self, reserva):
         self.__reservas.append(reserva)
+        print(f"{reserva} adicionada com sucesso.")
 
     def registrar_hospede(self, hospede):
         self.__hospedes.append(hospede)
+        print("Hóspede cadastrado com sucesso.")
 
     def registar_funcionario(self, funcionario):
         self.__funcionarios.append(funcionario)
@@ -29,8 +31,8 @@ class Hotel():
     def get_quartos(self):
         return self.__quartos
 
-    def listar_reservas(self):
-        pass
+    def get_reservas(self):
+        return self.__reservas
 
     def get_funcionarios(self):
         return self.__funcionarios
