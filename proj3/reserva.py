9bcd7306723e5ad409df2058ccbac3d0a20d6ad2
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-05-28 15:00:29 -0300

    Add get quarto method

--- proj3/reserva.py
+++ proj3/reserva.py
@@ -3,5 +3,8 @@ class Reserva():
         self.__hospede = hospede
         self.__quarto = quarto
 
+    def get_quarto(self):
+        return self.__quarto
+
     def __str__(self):
         return f"Reserva do {self.__quarto} por {self.__hospede}"
