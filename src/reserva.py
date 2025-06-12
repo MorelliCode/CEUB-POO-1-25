db230f5e4fee2a2e84f76ea632df75509080ec74
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-06-12 11:05:42 -0300

    Remove old files

--- src/reserva.py
+++
@@ -1,7 +0,0 @@
-class Reserva:
-    def __init__(self, hospede, quarto):
-        self.__hospede = hospede
-        self.__quarto = quarto
-
-    def __str__(self):
-        return f"reserva: {self.__quarto} - {self.__hospede}"
